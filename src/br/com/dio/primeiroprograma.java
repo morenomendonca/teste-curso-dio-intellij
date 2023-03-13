d18ddfe6424cf86e9d1a5957bb0f1e91c2b890b5
moreno <morenoarthur52@gmail.com>
2023-03-13 19:44:07 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        int a = 2;
+        int b= 5;
+        System.out.println("Hello World!" + (a+b));
+
+        Gato gato = new Gato("Roberto Carlos", "Amaretto", 2);
+
+        System.out.println(gato);
+    }
+
+}
